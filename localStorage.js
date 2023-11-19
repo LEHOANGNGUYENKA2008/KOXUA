df1f8d16daef0bc45e0e4ffd663bdfce9648e618
LEHOANGNGUYENKA2008 <flashyflash2008@gmail.com>
2023-11-19 19:15:55 +0700

    first commit

---
+++ localStorage.js
@@ -0,0 +1,10 @@
+// setItem("key", "value")
+localStorage.setItem("school", "MindX Technology School");
+localStorage.setItem("schoolname", "MindX Tech");
+
+// get Item("key")
+let name = localStorage.getItem("school");
+console.log(name);
+
+// removeItem("key")
+localStorage.removeItem("schoolname");
